8500d8aaeaadd1c08ff630f22c461e37efebb7a8
charity <charity23000@student.hamk.fi>
2023-10-09 14:15:53 +0300

    Initial commit

---
+++ exercise.java
@@ -0,0 +1,11 @@
+
+public class exercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String = printer app;
+		System.out.println("printer app");
+
+	}
+
+}
